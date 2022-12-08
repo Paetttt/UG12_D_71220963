a65b96e47a15b822cd59c018c304d496ef475ff7
Paetttt <118429357+Paetttt@users.noreply.github.com>
2022-12-08 16:07:59 +0700

    Add files via upload

--- soal2_D_71220963.py
+++ soal2_D_71220963.py
@@ -3,15 +3,21 @@ print("Pilihan Model Matematika")
 print("1. Perkalian")
 print("2. Pembagian")
 
-pilihan = input("Pilih model Matematika")
+pilihan = ("Pilih model Matematika")
 a = (input("Masukan model matematika yang diinginkan 1/2: "))
 if pilihan == '1':
    print("Perkalian")
-b = (input("Menampilkan table matematika dari angka: "))
-if pilihan == '2':
-   print("Pembagian")
+elif pilihan == '2':
+    print("Pembagian")
+else:
+    print("Pilihan tidak tersedia jangan ngasal:)")
+
+a = int(input("Menampilkan table matematika dari angka: "))
 
 for i in range(1, 11):
-    if i == 8 :
-        continue
-    print(i, "x",i, "=", i*i)
+    hasil = a * i
+    print(a, "x",i, "=", hasil)
+
+for i in range(1, 11):
+    hasil = a * i
+    print(a, "/",i, "=", hasil)
